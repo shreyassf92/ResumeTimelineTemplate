52edcc5f2aeaef652861b8162d821eb6f521f435
shreyas surendra <shreyas.sj3@gmail.com>
2021-02-22 16:21:07 -0500

    removed starter code and added files for TimelineCard component

---
+++ src/components/TimelineCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Classes from "./TimelineCard.css";
+
+const timelinecard = () => {
+
+    return (
+        <div className={Classes.container}>
+
+        </div>
+    );
+}
+
+export default timelinecard;
