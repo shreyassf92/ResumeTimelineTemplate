13bf094186699ad6806b83893309939802ded003
shreyas surendra <shreyas.sj3@gmail.com>
2021-02-22 17:17:12 -0500

    added component TimelineCard

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <TimelineCard />
+        <TimelineCard title="Software Engineer"
+          org="Mphasis"
+          duration="March 2015 - July 2019"
+          place="Bengaluru, India"
+          description="point1.point2.point3" />
       </div>
     );
   }
--- src/components/TimelineCard.js
+++ src/components/TimelineCard.js
@@ -1,11 +1,37 @@
 import React from "react";
 import Classes from "./TimelineCard.css";
 
-const timelinecard = () => {
+const timelinecard = (props) => {
+
+    let desc = props.description;
+    console.log(desc);
+    var n = 0;
+    let descList = desc.split('.').map(d => {
+        return (<li key={n++}>{d.trim()}</li>)
+    });
 
     return (
         <div className={Classes.container}>
+            <div className={Classes.head}>
+                <div className={Classes.head_col1}>
+                    <h2>{props.title}</h2>
+                    <h3>{props.org}</h3>
+                </div>
+                <div className={Classes.head_col2}>
+                    <p>{props.duration}</p>
+                    <p>{props.place}</p>
+                </div>
+            </div>
+            <div className={Classes.body}>
+                <ul>
+                    {descList}
 
+                    {/* <li>point 1</li>
+                    <li>point 2</li>
+                    <li>point 3</li>
+                    <li>point 4</li> */}
+                </ul>
+            </div>
         </div>
     );
 }
