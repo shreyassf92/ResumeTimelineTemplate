3a2c38f68f843aefa83bb4446c60209da79b9c7f
shreyas surendra <shreyas.sj3@gmail.com>
2021-02-24 22:55:31 -0500

    added toggle button to TimelineCard compoenent

--- src/App.js
+++ src/App.js
@@ -9,21 +9,21 @@ class App extends Component {
       <div className="App">
 
         <Timeline>
+          <TimelineCard title="Software Development Engineer – Intern"
+            org="Advancing Technologies"
+            duration="February 2021 – May 2021"
+            place="Orlando, Florida"
+            description="Develop an android mobile application that provides performance analytics to athletes in combat sports. Implement components that establish connectivity between the app, wearable device, and analytics engine" />
+          <TimelineCard title="Information Technology Intern"
+            org="FedEx Services"
+            duration="August 2020 - November 2020"
+            place="Collierville, Tennessee"
+            description="Design and develop a software solution that supports the organization’s transition to Software Defined Networks. Participate in agile ceremonies and meetings with different stakeholders to gather requirements. Provide presentations to management to encourage the usage of the tool" />
           <TimelineCard title="Software Engineer"
             org="Mphasis"
             duration="March 2015 - July 2019"
             place="Bengaluru, India"
-            description="point1.point2.point3" />
-          <TimelineCard title="Software Engineer"
-            org="Mphasis"
-            duration="March 2015 - July 2019"
-            place="Bengaluru, India"
-            description="point1.point2.point3" />
-          <TimelineCard title="Software Engineer"
-            org="Mphasis"
-            duration="March 2015 - July 2019"
-            place="Bengaluru, India"
-            description="point1.point2.point3" />
+            description="Designed and developed software components for a windows desktop application, FedEx Ship Manager, an end-to-end shipping application. Developed new features of the application that improved user experience, increased customer adoption and enabled product rationalization. Designed and developed a web application, an interface to the TFS version control web API; empowered the testing team to effortlessly track the data file changes in every new build and saved 2% of the team’s effort per day. Designed and developed a unit test execution tool to fit into the architecture that was employed; helped developers to test their code readily with its ability to isolate each test module’s execution environment and effortlessly owing to its user-friendly setup. Spearheaded a team of 3 for a unit testing project; achieved 70% code coverage and a significant improvement in software quality and build turnover time. Designed and developed an application to decrypt and display the content of an application data file used by multiple development and testing teams to analyze bugs; improved teams’ efficiency in detecting and fixing bugs. Worked on a Proof of Concept for developing a voice interface to the FedEx Tracking Web API using Alexa skill kit; presented the organization with a new opportunity to develop a voice driven app as an interface to tracking service. Awarded the summit award twice in a row for my contribution to the team and the organization’s innovation portal" />
         </Timeline>
 
       </div>
--- src/components/TimelineCard.js
+++ src/components/TimelineCard.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Classes from "./TimelineCard.css";
 
 const timelinecard = (props) => {
 
-    let desc = props.description;
-    console.log(desc);
+    console.log("rendered card");
+
     var n = 0;
-    let descList = desc.split('.').map(d => {
-        return (<li key={n++}>{d.trim()}</li>)
+    let descList = props.description.split('.').map(d => {
+        return (<li key={n++}><p>{d.trim()}</p></li>)
     });
+
+    const [isCardOpen, setCount] = useState(false);
+
+    const toggleCard = () => {
+        setCount(!isCardOpen);
+    }
 
     return (
         <div className={Classes.container}>
@@ -18,20 +24,27 @@ const timelinecard = (props) => {
                     <h3>{props.org}</h3>
                 </div>
                 <div className={Classes.head_col2}>
-                    <p>{props.duration}</p>
-                    <p>{props.place}</p>
+                    <h4>{props.duration}</h4>
+                    <h4>{props.place}</h4>
                 </div>
             </div>
-            <div className={Classes.body}>
-                <ul>
-                    {descList}
 
-                    {/* <li>point 1</li>
-                    <li>point 2</li>
-                    <li>point 3</li>
-                    <li>point 4</li> */}
-                </ul>
-            </div>
+            {isCardOpen &&
+                <div className={Classes.body}>
+                    <ul>
+                        {descList}
+                    </ul>
+                </div>
+            }
+
+            <button className={Classes.expand} onClick={toggleCard}>
+                {!isCardOpen &&
+                    <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" viewBox="0 0 24 24" fill="black" width="20px" height="20px"><g><path d="M0,0h24v24H0V0z" fill="none" /></g><g><g><g><path d="M7.71,9.29l3.88,3.88l3.88-3.88c0.39-0.39,1.02-0.39,1.41,0l0,0c0.39,0.39,0.39,1.02,0,1.41l-4.59,4.59 c-0.39,0.39-1.02,0.39-1.41,0L6.29,10.7c-0.39-0.39-0.39-1.02,0-1.41l0,0C6.68,8.91,7.32,8.9,7.71,9.29z" /></g></g></g></svg>
+                }
+                {isCardOpen &&
+                    <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" viewBox="0 0 24 24" fill="black" width="20px" height="20px"><g><path d="M0,0h24v24H0V0z" fill="none" /></g><g><g><g><path d="M7.71,15.29l3.88-3.88l3.88,3.88c0.39,0.39,1.02,0.39,1.41,0l0,0c0.39-0.39,0.39-1.02,0-1.41l-4.59-4.59 c-0.39-0.39-1.02-0.39-1.41,0l-4.59,4.59c-0.39,0.39-0.39,1.02,0,1.41l0,0C6.68,15.67,7.32,15.68,7.71,15.29z" /></g></g></g></svg>
+                }
+            </button>
         </div>
     );
 }
